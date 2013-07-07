76df38b522b1c997ab45182be9582558cc5f692e
Marlon Vinan <santiago@ubuntu-marlon.(none)>
2013-07-07 17:36:20 -0500

    Modified web.js

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer = fs.readFileSync("index.html");
+
+var msg = buffer.toString('utf-8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2 !');
+  response.send(msg);
 });
 
 var port = process.env.PORT || 5000;
